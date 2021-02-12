4736cbdc9caebd7902082f2b6b255a56a38777e8
cholin <nico@geyso.net>
2021-02-12 15:34:16 +0100

    requirements - add wheel and fix name

--- src/cffi/setup.py
+++ src/cffi/setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 source_path = Path(__file__).resolve()
 source_dir = source_path.parent / '..' / '..'
 setup(
-    name = 'signal-protocol-cffi',
+    name = 'signal_protocol_cffi',
     version = '0.1',
     cffi_modules=["./build.py:ffi"],
     install_requires = ['cffi']
